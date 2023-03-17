f10bd9432e1cabb08dcf2486c5c2df0e1ab01d3f
leonardohendler <leonardohhendler@rede.ulbra.br>
2023-03-17 15:25:46 -0300

    Update exercicios2.py

--- Matérias/Algoritmo e Programação/Atividades.PY/exercicios2.py
+++ Matérias/Algoritmo e Programação/Atividades.PY/exercicios2.py
@@ -46,21 +46,84 @@
 
 # 7. Uma loja de roupas está com uma promoção de 20% de desconto em todos os produtos. Uma blusa
 # custava R$ 80, quanto ela custará agora?
-
+# blusa= 80
+# desconto= 20
+# valor_com_desconto= blusa - (desconto*blusa/100)
+# print(f'O valor da blusa com desconto será: {valor_com_desconto}')
 
 # 8. Um triângulo equilátero tem lado de medida 6cm. Calcule a sua área.
+
+# lado_do_triangulo= 6
+# média_do_triangulo= lado_do_triangulo*3
+# print(f'A média do triangulo equilatero é: {média_do_triangulo}')
+
 # 9. Um homem percorreu 10km em uma hora e meia. Qual é a sua velocidade média em km/h?
+
+# distancia_percorrida= 10
+# tempo= 1.5
+# velocidade_média= distancia_percorrida/tempo
+# print(f'A velocidade média foi:{velocidade_média}')
+
 # 10. Um jardineiro está plantando uma cerca viva que tem 25 metros de comprimento. Se ele precisa
 # plantar uma muda a cada 50 centímetros, quantas mudas ele precisará?
+
+# cerca_viva= 25
+# distancia_entre_as_mudas= 50
+# mudas_necessarias= (cerca_viva*100)/50
+# print(f'O jardineiro precisará de: {mudas_necessarias} mudas')
+
 # 11. Uma escola de inglês oferece um curso de 6 meses que custa R$ 2.400. Qual é o valor mensal do
 # curso?
+
+# valor_total_curso=float (2400.00)
+# quantidade_meses= float (6)
+# valor_mensal_curso= valor_total_curso/quantidade_meses
+# print(f'O valor mensal do curso é: R$ {valor_mensal_curso}')
+
 # 12. Calcule a área de um círculo de raio 4cm. Considere a fórmula Área = π x raio2.
+
+# raio_circulo= 4
+# pi= 3.14
+# area_circulo= pi*(raio_circulo**2)
+# print(f'O valor da area do circulo é: {area_circulo}')
+
 # 13. Uma loja de brinquedos está com uma promoção de 30% de desconto em um boneco que custa R$ 60.
 # Qual é o preço do boneco com desconto?
+
+# valor_boneco= 60
+# desconto= 30
+# boneco_com_desconto= valor_boneco - (desconto*valor_boneco)/100
+# print(f'O valor do produto com desconto é: R$ {boneco_com_desconto} reais')
+
 # 14. Um carro faz 12km por litro de gasolina. Se ele percorrer uma distância de 360km, quantos litros de
 # gasolina ele gastará?
+
+# média_carro= 12
+# distancia_percorrida= 360
+# litros_necessarios= distancia_percorrida/média_carro
+# print(f'O carro gastará: {litros_necessarios} litros')
+
+
 # 15. Uma pessoa está organizando uma festa e precisa de 1,5 litros de refrigerante para cada convidado. Se
 # ela convidou 30 pessoas, quantos litros de refrigerante ela precisará?
+
+# litro_por_convidado= 1.5
+# numero_convidados= 30
+# litros_refrigerante= litro_por_convidado*numero_convidados
+# print(f'Essa pessoa precisará de {litros_refrigerante} litros de refrigerante ')
+
 # 16. Calcule o perímetro de um quadrado de lado 5cm.
+
+# lado_quadrado= 5
+# perimetro_quadrado= lado_quadrado*4
+# print(f' O perimetro do quadrado é: {perimetro_quadrado}')
+
 # 17. Uma pessoa tem R$ 500 em sua conta corrente e precisa pagar uma conta de R$ 150 e
 # outra de R$ 300. Qual é o saldo final da conta corrente?
+
+# saldo_conta= 500
+# conta1= 150
+# conta2= 300
+# saldo_final= saldo_conta -(conta1+conta2)
+# print(f'O saldo em conta restante é: R$ {saldo_final} reais')
+
