f3262695b3a6b45b0eedb56b3df1fc8217ff4360
leonardohendler <leonardohhendler@rede.ulbra.br>
2023-03-25 10:49:37 -0300

    exercicios3.py

---
+++ Matérias/Algoritmo e Programação/Atividades.PY/exercicios3.py
@@ -0,0 +1,69 @@
+# 1. Você está desenvolvendo um programa para calcular a corrente elétrica em um circuito elétrico. O
+# usuário deve informar a resistência elétrica e a tensão elétrica do circuito. Escreva um algoritmo que
+# calcule e imprima a corrente elétrica do circuito, de acordo com a lei de Ohm.
+
+
+
+# 2. Você está desenvolvendo um programa que verifica se uma pessoa pode dirigir ou não. Para isso,
+# você precisa solicitar a idade da pessoa e verificar se ela é maior ou igual a 18 anos. Se a idade for
+# menor que 18, o programa deve informar que a pessoa não pode dirigir, caso contrário, o programa
+# deve informar que a pessoa pode dirigir.
+
+
+
+# 3. Você está desenvolvendo um programa que verifica se um número é par ou ímpar. Para isso, você
+# precisa solicitar o número e verificar se ele é divisível por 2. Se o número for divisível por 2, o
+# programa deve informar que o número é par, caso contrário, o programa deve informar que o número
+# é ímpar.
+
+
+
+# 4. Você está desenvolvendo um programa para calcular a altura máxima e o tempo de queda de um
+# objeto em um lançamento vertical. O usuário deve informar a altura inicial do objeto. Assuma que a
+# aceleração da gravidade é igual a 9,81 m/s2. Escreva um algoritmo que calcule e imprima a altura
+# máxima e o tempo de queda do objeto.
+
+
+
+# 5. Você está desenvolvendo um programa para calcular a distância percorrida por um objeto em um
+# movimento uniformemente acelerado. O usuário deve informar a velocidade inicial, a aceleração e o
+# tempo de deslocamento. Escreva um algoritmo que calcule e imprima a distância percorrida pelo
+# objeto.
+
+
+
+# 6. Você está desenvolvendo um programa para classificar triângulos de acordo com as medidas de
+# seus lados. O usuário deve informar as medidas dos três lados do triângulo. Escreva um algoritmo
+# que determine e imprima se o triângulo é equilátero, isósceles ou escaleno.
+
+
+
+# 7. João é um médico que trabalha em uma clínica especializada em saúde preventiva. Ele está
+# desenvolvendo um programa para calcular o IMC (Índice de Massa Corporal) de seus pacientes. O
+# IMC é uma medida que relaciona o peso e a altura de uma pessoa e é utilizada para verificar se ela
+# está com o peso ideal. João sabe que, para calcular o IMC, ele precisa da altura e do peso do
+# paciente. Escreva um algoritmo em Python que ajude João a calcular o IMC de seus pacientes.
+
+
+
+# 8. Maria é uma estudante de física que está desenvolvendo um programa para converter uma
+# temperatura de Celsius para Fahrenheit. Ela sabe que muitas vezes precisa realizar essa conversão
+# em seus estudos, mas a fórmula matemática pode ser confusa. Por isso, ela está desenvolvendo um
+# programa em Python que simplifica esse processo. Escreva um algoritmo que ajude Maria a
+# converter uma temperatura de Celsius para Fahrenheit.
+
+
+
+# 9. Pedro é um estudante de matemática que está desenvolvendo um programa para calcular o volume
+# de uma esfera. Ele sabe que o cálculo pode ser um pouco complicado, mas também é muito
+# importante em diversas áreas da matemática e da física. Pedro está animado em desenvolver esse
+# programa em Python para ajudar outros estudantes a entenderem melhor a fórmula. Escreva um
+# algoritmo que ajude Pedro a calcular o volume de uma esfera.
+
+
+
+# 10. Ana é uma professora de matemática que está desenvolvendo um programa para calcular a média
+# aritmética de uma lista de números. Ela sabe que, para calcular a média, é necessário somar todos
+# os números da lista e dividir pelo total de números. Ana quer desenvolver um programa em Python
+# que possa ser utilizado por seus alunos para facilitar esse processo. Escreva um algoritmo que ajude
+# Ana a calcular a média aritmética de uma lista de números.
