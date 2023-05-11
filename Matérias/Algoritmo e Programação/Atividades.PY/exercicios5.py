dee47c5170939f93fcf5cd95144e48640ac7c5c5
leonardohendler <leonardohhendler@rede.ulbra.br>
2023-05-10 21:04:07 -0300

    Create exercicios5.py

---
+++ Matérias/Algoritmo e Programação/Atividades.PY/exercicios5.py
@@ -0,0 +1,55 @@
+# 1. Escreva um programa que receba uma lista de números do usuário e imprima a
+# lista na tela.
+# 2. Escreva um programa que receba uma lista de nomes do usuário e imprima cada
+# nome em uma linha separada.
+# 3. Escreva um programa que receba uma lista de números do usuário e calcule a
+# soma de todos os números presentes na lista.
+# 4. Escreva um programa que receba uma lista de números do usuário e calcule a
+# média de todos os números presentes na lista.
+# 5. Escreva um programa que receba uma lista de números do usuário e imprima
+# apenas os números pares presentes na lista.
+# 6. Escreva um programa que receba uma lista de números do usuário e imprima
+# apenas os números ímpares presentes na lista.
+# 7. Escreva um programa que receba uma lista de números do usuário e imprima
+# apenas os números que são múltiplos de 3 e 5 simultaneamente.
+# 8. Escreva um programa que receba uma lista de números do usuário e imprima a
+# lista em ordem crescente.
+# 9. Escreva um programa que receba uma lista de números do usuário e imprima a
+# lista em ordem decrescente.
+# 10.Escreva um programa que receba uma lista de números do usuário e verifique se
+# um número específico está presente na lista.
+
+
+
+
+
+# 1.Escreva um programa que receba uma lista de números do usuário e retorne o maior número
+# presente na lista.
+# 2. Escreva um programa que receba uma lista de números do usuário e retorne o menor número
+# presente na lista.
+# 3. Escreva um programa que receba uma lista de números do usuário e retorne a soma de todos os
+# números presentes na lista.
+# 4. Escreva um programa que receba uma lista de números do usuário e retorne a quantidade de
+# números pares presentes na lista.
+# 5. Escreva um programa que receba uma lista de números do usuário e retorne a quantidade de
+# números ímpares presentes na lista.
+# 6. Escreva um programa que receba uma lista de números do usuário e retorne uma lista com
+# apenas os números pares presentes na lista.
+# 7. Escreva um programa que receba uma lista de números do usuário e retorne uma lista com
+# apenas os números ímpares presentes na lista.
+# 8. Escreva um programa que receba uma lista de nomes do usuário e retorne o nome mais longo
+# presente na lista.
+# 9. Escreva um programa que receba uma lista de números do usuário e retorne a média dos
+# números presentes na lista.
+# 10. Escreva um programa que receba uma lista de números do usuário e retorne uma lista com
+# apenas os números que são múltiplos de 3 e 5 simultaneamente.
+# 11. Escreva um programa que receba uma lista de nomes do usuário e determine se todos os
+# nomes possuem a mesma quantidade de caracteres.
+# 12. Escreva um programa que receba uma lista de números do usuário e retorne uma lista com
+# apenas os números ímpares presentes na lista, utilizando um loop while.
+# 13. Escreva um programa que receba uma lista de números do usuário e retorne a lista em ordem
+# crescente.
+# 14. Escreva um programa que receba uma lista de números do usuário e retorne a lista em ordem
+# decrescente.
+# 15. Escreva um programa que receba uma lista de números do usuário e retorne uma lista sem
+# duplicatas.
