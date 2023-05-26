fc6fae4dac6b9d363ca951a1549cc6d91a6b57f0
leonardohendler <leonardohhendler@rede.ulbra.br>
2023-05-25 23:34:16 -0300

    att

--- Matérias/Introdução Web/JavaScript/exer2.js
+++ Matérias/Introdução Web/JavaScript/exer2.js
@@ -1,1 +1,15 @@
 // 2 - Faça um script que leia um número e mostre uma mensagem indicando se este número é par ou ímpar e se é positivo ou negativo.
+
+// let num = Number(prompt("Digite um numero"));
+// if (num % 2 == 0  && num > 0) {
+//     alert("O numero é Par!")
+//     alert("O numero é positivo")
+// }
+// else if (num % 2 == 0 && num < 0) {
+//     alert("O numero é Par!")
+//     alert("O numero é negativo")
+// }
+// else{
+//     alert("O numero é impar!")
+// }
+    
--- Matérias/Introdução Web/JavaScript/exer3.js
+++ Matérias/Introdução Web/JavaScript/exer3.js
@@ -1,1 +1,10 @@
 // 3 - Faça um script que leia o nome e a idade de uma pessoa e informe se a pessoa é maior de idade ou não. Exemplo: ‘João você tem 30 anos, é maior de idade’.
+// let nome= prompt("Digite seu nome");
+// let idade= prompt("Digite sua idade!");
+
+// if (idade >= 18){
+//     alert(`${nome} você tem ${idade}, é maior de idade`)
+// }
+// else{
+//     alert(`${nome} você tem ${idade}, é menor de idade`)
+// }
