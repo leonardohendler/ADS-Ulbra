c3d70d5c3ab343239e16b2d3aea267ad7b7afea6
leonardohendler <leonardohhendler@rede.ulbra.br>
2023-06-16 20:11:29 -0300

    atividades python

---
+++ Matérias/Algoritmo e Programação/Atividades.PY/funcao.py
@@ -0,0 +1,64 @@
+# def funcao(par1,par2):
+#     total=par1*par2
+#     return total
+    
+# print(funcao(2,4))
+
+# ___________________________________________________________________
+
+
+# def saudacao(nome):
+#     print(f"Ola {nome}! Seja Bem-vindo")
+
+# saudacao("Leonardo")    
+# saudacao("Heloisa")
+
+# ___________________________________________________________________
+
+
+# def calcular_media(nota1, nota2):
+#     media= (nota1+nota2)/2
+#     return media
+
+# num1= int(input("Digite um numero\n"))
+# num2= int(input("Digite um numero\n"))
+
+# resultado=calcular_media(num1,num2)
+# print(f"O resultado da média é: {resultado}")
+
+
+# ___________________________________________________________________
+
+# def soma (a,b):
+#     return a + b
+
+# resultado = soma(2,3)
+# print(resultado)
+# # uma maneira de fazer
+
+# print(f"O resultado da média é: {soma(2,3):.2f}")
+# # outra maneira de fazer, menos codigo    
+
+# ___________________________________________________________________
+
+# def soma (a,b):
+#     somar = a + b
+#     return somar
+# resultado=soma(2,3)
+# print(resultado)
+
+
+# ___________________________________________________________________
+
+
+# def divisao (a,b):
+#     dividir = a/b
+#     resto = a % b
+#     return dividir, resto
+
+# resultado1,resultado2= divisao(10,5)
+# print(resultado1, resultado2)
+
+
+# ___________________________________________________________________
+
