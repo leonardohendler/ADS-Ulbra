ebf29f5b59b32e2425ab6f96d4c42e49a282fb15
leonardohendler <leonardohhendler@rede.ulbra.br>
2023-06-26 20:57:47 -0300

    finalizando TDE/AS

--- Matérias/Introdução Web/TDE.AS/Lista de Tarefas/script.js
+++ Matérias/Introdução Web/TDE.AS/Lista de Tarefas/script.js
@@ -4,16 +4,21 @@ function adicionar() {
   
     if (tarefa !== '') {
       li.textContent = tarefa;
-      const ol = document.getElementById('pendentes');
-      ol.appendChild(li);
+      const ul = document.getElementById('pendentes');
+      ul.appendChild(li);
     } else {
       alert('Informe uma tarefa!');
     }
   
     const btnRemove = document.createElement('button');
-    btnRemove.textContent = 'remover';
+    btnRemove.textContent = 'Excluir';
     btnRemove.type = 'button';
     btnRemove.style.marginLeft = '20px';
+    btnRemove.style.color= '#FFF'
+    btnRemove.style.width= '100px'
+    btnRemove.style.height='30px'
+    btnRemove.style.background = '#191818'
+    btnRemove.style.borderRadius = '5px'
     btnRemove.addEventListener('click', function () {
       li.remove();
     });
@@ -23,6 +28,11 @@ function adicionar() {
     btnConcluido.textContent = 'Concluído';
     btnConcluido.type = 'button';
     btnConcluido.style.marginLeft = '20px';
+    btnConcluido.style.color= '#FFF'
+    btnConcluido.style.width= '100px'
+    btnConcluido.style.height='30px'
+    btnConcluido.style.background = '#191818'
+    btnConcluido.style.borderRadius = '5px'
     btnConcluido.addEventListener('click', function () {
         concluido(li);
     });
@@ -30,6 +40,9 @@ function adicionar() {
   
     document.getElementById('tarefa').value = '';
     document.getElementById('tarefa').focus();
+
+    
+
   }
   
   function concluido(li) {
@@ -37,8 +50,18 @@ function adicionar() {
     concluidos.appendChild(li); 
     
   }
-  
 
+  function manipulandoClasseSun(){
+    const body = document.querySelector('body')
+    body.classList.add('fundobranco')
+
+  }
+  
+  function manipulandoClasseMoon(){
+    const body = document.querySelector('body')
+    body.classList.remove('fundobranco')
+
+  }
 
 
 
